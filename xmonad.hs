caccf74b89b4650f75f5a1e6cf5ddcaf82ef2c7e
KireinaHoro <xubin990510@gmail.com>
2017-07-07 21:20:12 +0800

    update scrot options

--- xmonad.hs
+++ xmonad.hs
@@ -6,19 +6,33 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+-------------------------------------------------
+-- Configuration
+
+myTerminal = "termite"
+-- Sleep for 1 sec so that xset can really shut down the screen
+myScreensaver = "xscreensaver-command -lock; sleep 1; xset dpms force off"
+mySelectScreenshot = "sleep 0.2; " ++ myScreenshot ++ " -s"
+myScreenshot = "scrot -e 'mv $f ~/Screenshots/'"
+myLauncher = "$(yeganesh -x -- -fn 'Source Code Pro-11')"
+myModMask = mod4Mask
+
+-------------------------------------------------
+
 main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ desktopConfig
         { manageHook = manageDocks <+> manageHook desktopConfig
-        , terminal = "termite"
+        , terminal = myTerminal
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 48
                         }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , modMask = myModMask   
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; sleep 1; xset dpms force off")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
+        [ ((myModMask .|. shiftMask, xK_z), spawn myScreensaver)
+        , ((controlMask, xK_Print), spawn mySelectScreenshot)
+        , ((myModMask, xK_p), spawn myLauncher)
+        , ((0, xK_Print), spawn myScreenshot)
         ]
