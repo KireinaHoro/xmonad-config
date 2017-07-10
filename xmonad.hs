2c95a2385dc5eef84235c16cbfe32df6c9108743
KireinaHoro <i@jsteward.moe>
2017-07-10 14:25:08 +0800

    add manageHook customizations

--- xmonad.hs
+++ xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Data.List(isInfixOf)
 
 -------------------------------------------------
 -- Configuration
@@ -17,17 +18,39 @@ myScreenshot = "scrot -e 'mv $f ~/Screenshots/'"
 myLauncher = "$(yeganesh -x -- -fn 'Source Code Pro-11')"
 myModMask = mod4Mask
 
+myWorkspaces = ["T", "W", "I", "M", "5", "6", "7", "8", "9", "0", "-", "="]
+
+myManageHook = composeAll . concat $
+     [ [ className =? "Firefox"          --> doShift "W" ]
+     , [ className =? "Termite"          --> doShift "T" ]
+     , [ className =? "TelegramDesktop"  --> doShift "I" ]
+     , [ fmap ( "Directory index of /Anime/" `isInfixOf` ) title --> doShift "M" ] -- doesn't work for now as Firefox changes the title *after* the window was created
+     , [(className =? "Firefox" <&&> resource =? "Places") --> doFloat]
+     , [(className =? "Firefox" <&&> resource =? "Browser") --> doFloat]
+
+       -- using list comprehensions and partial matches
+     , [ className =? c --> doFloat | c <- myFloatsC ]
+     , [ fmap ( c `isInfixOf` ) className   --> doFloat | c <- myMatchAnywhereFloatsC ]
+     , [ fmap ( c `isInfixOf` ) title       --> doFloat | c <- myMatchAnywhereFloatsT ] 
+
+     , [ manageDocks ]
+     ]
+    where myFloatsC = ["Pavucontrol", "feh", "Lxappearance", "Xscreensaver-demo"]
+          myMatchAnywhereFloatsC = ["Google"]
+          myMatchAnywhereFloatsT = ["Directory index of /Anime/"] -- same as above
+
 -------------------------------------------------
 
 main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ desktopConfig
-        { manageHook = manageDocks <+> manageHook desktopConfig
+        { manageHook = myManageHook <+> manageHook desktopConfig
+        , workspaces = myWorkspaces
         , terminal = myTerminal
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 48
+                        , ppTitle = xmobarColor "green" "" . shorten 45
                         }
         , modMask = myModMask   
         } `additionalKeys`
